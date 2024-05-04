2515948c981e54b5fb204724fa27eef98d6bbab6
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-05-04 22:00:04 +0600

    update code

--- src/components/blog/BlogSingle.vue
+++ src/components/blog/BlogSingle.vue
@@ -1,12 +1,55 @@
 <script setup>
+import { useRoute } from "vue-router";
+import { fn } from "../../data.js";
+import { ref } from "vue";
 
+const route = useRoute();
+const postDetails = ref({});
+
+const getPostDetails = () => {
+  const res = fn.fetchPublicApi(`/posts/${route.params.id}`, "get");
+  res.then((response) => {
+    postDetails.value = response;
+  });
+};
+
+getPostDetails();
+
+// watch(route, () => {
+//   getPostDetails();
+// });
 </script>
-
 <template>
-  <div>
-    <div class="container"><h1>This is blog single page</h1></div>
+  <!-- <h2>Related Posts</h2>
+  <router-link class="block text-xl mt-4 lg:inline-block lg:mt-0 text-black mr-10" to="/blog/1">Blog 1</router-link>
+  <router-link class="block text-xl mt-4 lg:inline-block lg:mt-0 text-black" to="/blog/2">Blog 2</router-link> -->
+  <div class="container mt-5">
+    <div class="card p-4">
+      <div class="">
+        <div class="card-title">
+          {{ postDetails.title }}
+        </div>
+        <p class="card-body">{{ postDetails.body }}</p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          v-for="tag in postDetails.tags"
+          :key="tag"
+          class="badge text-bg-warning me-2"
+          >#{{ tag }}</span
+        >
+      </div>
+      <img
+        class="w-100"
+        src="https://source.unsplash.com/random/1000x300"
+        alt="Sunset in the mountains"
+      />
+    </div>
   </div>
 </template>
 
 <style scoped>
+img{
+  object-fit: cover;
+}
 </style>
--- src/components/parts/BlogCard.vue
+++ src/components/parts/BlogCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, computed } from "vue";
+import {  computed } from "vue";
 
 const props = defineProps({
   post: Object,
@@ -13,10 +13,9 @@ const randomImageUrl = computed(() => {
   try {
     // Generate a random image URL
     const randomId = Math.floor(Math.random() * 1000); // Adjust as needed
-    return `https://picsum.photos/id/${randomId}/200/300`; // Adjust width and height as needed
+    return `https://picsum.photos/id/${randomId}/700/300`; // Adjust width and height as needed
   } catch (error) {
     console.error('Error generating random image URL:', error);
-    // Return a fallback image URL or handle the error in another way
     return 'fallback-image-url.jpg';
   }
 });
@@ -42,7 +41,7 @@ const randomImageUrl = computed(() => {
             <span
               v-for="tag in post.tags"
               :key="tag"
-              class="badge text-bg-light me-2"
+              class="badge text-bg-dark me-2"
               >#{{ tag }}</span
             >
           </div>
